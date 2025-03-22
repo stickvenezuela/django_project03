0b474546f717ad42f2a4fcd6ae8540494fa58867
josecabellog <josegregoriocabellog@gmail.com>
2025-03-22 09:51:38 -0400

    el css

--- django_project/settings.py
+++ django_project/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS=[BASE_DIR/'static/']
+os.path.join (BASE_DIR, 'static')
+STATIC_ROOT=BASE_DIR/'staticfiles'
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media/'
 
